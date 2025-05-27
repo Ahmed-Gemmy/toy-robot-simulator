6336f30812073a62f27184f9413caf7796ab7ebc
Ahmed Ali <ahmedgali.86@gmail.com>
2025-05-27 10:09:31 +0100

    direction.py pass tests

--- direction.py
+++ direction.py
@@ -1,2 +1,23 @@
-class Direction:
-    pass
+from enum import Enum
+
+class Direction(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+    @classmethod
+    def left(cls, current):
+        """Rotate the direction 90 degrees to the left (counter-clockwise)."""
+        return cls((current.value -1) % 4)
+
+    @classmethod
+    def right(cls, current):
+        """Rotate the direction 90 degrees to the right (clockwise)."""
+        return cls((current.value +1) % 4)
+
+    def __str__(self):
+        return self.name
+
+
+
