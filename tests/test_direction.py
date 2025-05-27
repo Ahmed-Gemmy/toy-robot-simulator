54db359542cffb46db6723564a2cd34a800c3719
Ahmed Ali <ahmedgali.86@gmail.com>
2025-05-27 10:07:50 +0100

    fixing right turn test

--- tests/test_direction.py
+++ tests/test_direction.py
@@ -10,10 +10,10 @@ class TestDirection(unittest.TestCase):
         self.assertEqual(Direction.left(Direction.EAST), Direction.NORTH)
 
     def test_right_turn(self):
-        self.assertEqual(Direction.left(Direction.NORTH), Direction.EAST)
-        self.assertEqual(Direction.left(Direction.WEST), Direction.NORTH)
-        self.assertEqual(Direction.left(Direction.SOUTH), Direction.WEST)
-        self.assertEqual(Direction.left(Direction.EAST), Direction.SOUTH)
+        self.assertEqual(Direction.right(Direction.NORTH), Direction.EAST)
+        self.assertEqual(Direction.right(Direction.EAST), Direction.SOUTH)
+        self.assertEqual(Direction.right(Direction.SOUTH), Direction.WEST)
+        self.assertEqual(Direction.right(Direction.WEST), Direction.NORTH)
 
 if __name__ == '__main__':
     unittest.main()
