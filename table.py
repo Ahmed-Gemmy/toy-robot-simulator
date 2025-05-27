e1ddda655eb136a677bd4eb2feb8ce800b2aba95
Ahmed Ali <ahmedgali.86@gmail.com>
2025-05-27 10:18:46 +0100

    table.py pass tests

--- table.py
+++ table.py
@@ -1,2 +1,7 @@
 class Table:
-    pass
+    def __init__(self, width=5, hight=5):
+        self.width = width
+        self.hight = hight
+
+    def is_valid_position(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.hight
