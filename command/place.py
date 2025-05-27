d521bb42cedbf4bb7f79a7c378d3e5d99b033aaf
Ahmed Ali <ahmedgali.86@gmail.com>
2025-05-27 10:49:24 +0100

    place.py passes tests

--- command/place.py
+++ command/place.py
@@ -1,4 +1,10 @@
 from command.base import Command
 
 class PlaceCommand(Command):
-    pass
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+
+    def execute(self, robot):
+        robot.place(self.x, self.y, self.direction)
